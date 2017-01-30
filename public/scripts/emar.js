080ab1553a41b3387ade75f19ab9d35dd21c29af
Kyle Housley <kyle@bill4time.com>
2017-01-30 12:04:28 -0800

    super barebones data driven survey machine

--- public/scripts/emar.js
+++ public/scripts/emar.js
@@ -1,6 +1,13 @@
 var $ = require("jquery");
+var surveyGen = require("./survey");
 
 
 $(function () {
-    alert("time to emar it up");
+    $.get("/surveys", function (surveys) {
+        var sHtml = surveyGen.getHtml(surveys[0]);
+
+        $("#main-container").html(sHtml);
+    });
+
+
 });
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+var surveys = require("./surveys/surveys");
 var PORT = 1337;//TODO some server config mightbe necessary
 
 app.use(express.static("public"));
 
 app.get('/', function (req, res) {
     res.sendFile("public/index.html");
+});
+
+app.get("/surveys/", function (req, res) {
+    res.status(200).json(surveys);
 });
 
 app.listen(PORT, function () {
