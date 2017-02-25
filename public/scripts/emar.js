c7a712fba0aff096d8bf9a07d2262bf29e1b0a22
Kyle Housley <kyle@bill4time.com>
2017-02-24 20:44:18 -0800

    quick hack on the UI

--- public/scripts/emar.js
+++ public/scripts/emar.js
@@ -1,13 +1,101 @@
 var $ = require("jquery");
-var surveyGen = require("./survey");
+var survey = require("./survey");
+var main;
+var count = 0;
 
+var questions = [{
+    q: "How stressed do you feel right now?"
+}, {
+    q: "What is your energy level right now?"
+}, {
+    q: "How is your mood right now?"
+}];
 
-$(function () {
-    $.get("/surveys", function (surveys) {
-        var sHtml = surveyGen.getHtml(surveys[0]);
+var responses = [
+    "Glad to hear that.",
+    "Glad to hear that.",
+    "I'm feeling about the same.",
+    "Oh no, I'm sorry to hear that."
+];
 
-        $("#main-container").html(sHtml);
+function greetings () {
+    main.html(survey.greeting());
+
+    main.find("button.hi").click(function () {
+        //Unhide greet text
+        main.find(".greeting-button").hide();
+        main.find(".greeting-text").removeClass("hidden");//play greeting audioi
     });
 
+    main.find("button.yes").click(function () {
+        //start survey
+        askQuestion();
+    });
+
+    main.find("button.no").click(function () {
+        //say That's okay. Find me later if you feel like talking. and then go back to greetings
+    });
+
+
+    //HI can I axe you a question?
+    //If yes, do ask
+    //else say it's fine when it's really not
+}
+
+function askQuestion () {
+
+    main.html(survey.getHtml(questions[count]));
+    main.find(".survey button").click(handleQuestionAnswered);
+}
+
+function map (i_end, out_end, i_value) {
+    var ratio = i_value / i_end;
+    var squashedValue = out_end * ratio;
+    return Math.round(squashedValue);
+}
+
+function another () {
+    main.find(".survey").html(
+        "Thanks for sharing that with me. Would you like to answer another question? <br />" +
+        "<button class='yes btn btn-default'>Yes</button><button class='no btn btn-default'>No</button>"
+    );
+
+    main.find(".yes").click(function () {
+        askQuestion();
+    });
+    main.find(".no").click(function () {
+        main.html("Thanks for sharing that with me, I hope to see you again.");
+        //do some set timeout to show greeting again, also show greeting again on screen touch. Whichever is first
+        //blow away screen listener when timeout expires and cancel timeout when screen is touched
+        //Thank for sharing with me
+    });
+}
+
+function handleQuestionAnswered () {
+    //squash 0-99 into 0 - 3 to select appropriate response
+    //show response
+    //ask if we may have another
+    var responseValue = map(99, 3, main.find(".leich").val());
+    var response = responses[responseValue];
+    var cont = count + 1 < responses.length;
+    main.html(survey.response(response, cont));
+    setTimeout(function () {
+        if (cont) {
+            count++;
+            askQuestion();
+            //hookup to yes and no
+        } else {
+            main.html("Thanks for sharing that with me, I hope to see you again.");
+            //do some set timeout to show greeting again, also show greeting again on screen touch. Whichever is first
+            //blow away screen listener when timeout expires and cancel timeout when screen is touched
+            //Thank for sharing with me
+        }
+    }, 3000);
+
+}
+
+$(function () {
+    main = $("#main-container");
+    greetings();
 
 });
--- public/scripts/survey.js
+++ public/scripts/survey.js
@@ -14,11 +14,36 @@ module.exports = {
      * @param surveyData.soundfilePath
      */
     getHtml: function (surveyData) {
-        return "<div class='survey'>" +
-            // "<audio src='" + surveyData.soundfilePath + "'></audio>" +
-            surveyData.questionText +
-            "<input type='range' min='0' max='100' step='1'/>" +
-            "<button>Submit</button>" +
+        return "<div class='row'>" +
+            "<div class='survey col-xs-6 col-xs-offset-2'>" +
+                // "<audio src='" + surveyData.soundfilePath + "'></audio>" +
+                surveyData.q +
+                "<input class='leich' type='range' min='0' max='100' step='1'/>" +
+                "<button class='btn btn-block btn-default'>Submit</button>" +
+            "</div>" +
         "</div>"
+    },
+
+    response: function (res, continueSurvey) {
+        return  "<div class='response row'>" +
+                    "<div class='survey col-xs-6 col-xs-offset-2'>" +
+                        "<div>" + res + "</div>" +
+                    "</div>" +
+                "</div>"
+    },
+
+    greeting: function () {
+        return "<div class='greeting-button row'>" +
+                    "<div class='col-xs-6 col-xs-offset-3'>" +
+                        "<button class='hi btn btn-block btn-default'>Press to say hi</button>" +
+                    "</div>" +
+                "</div>" +
+                "<div class='greeting-text row hidden'>" +
+                    "<div class='survey col-xs-6 col-xs-offset-2'>" +
+                        "<div>Hi, nice to meet you! I'm EMAR. <br />" +
+                            "I'm excited to hear about your day. May I ask you a question?</div>" +
+                        "<button class='yes btn btn-default'>Yes</button><button class='no btn btn-default'>No</button>" +
+                    "</div>" +
+                "</div>";
     }
 };
