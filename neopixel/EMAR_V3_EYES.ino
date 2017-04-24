f0812357eac59a50f8b94e65b5c169ee13f1a7a6
joshuapyao <joshuapyao@gmail.com>
2017-04-24 14:44:56 -0700

    now accepts user control from serial input

--- neopixel/EMAR_V3_EYES.ino
+++ neopixel/EMAR_V3_EYES.ino
@@ -1,8 +1,7 @@
 //
-//EMAR V2 Eyes and Mp3
-//Kristine Kohlhepp
-//June 2016
-//Code written with reference to Arduino Master Writer Tutorial
+//EMAR V2 Eyes
+//Joshua Yao
+//April 2017
 
 
 
@@ -17,157 +16,15 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 16, PIN,
                             NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
                             NEO_GRB            + NEO_KHZ800);
                             
-int on = 255;
-int off = 0;
 unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
 int currentEyes[8][8]; // records current eyes for blinking
+int eyesCalled[8][8]; // eye array sent in from serial
+
+
 // End Neopixel setup-----------------------
 // Neopixel functions-----------------------
-//happy
-void happy() {
-  int eyes[8][8] = {
-                    {off, off, on, on, on, on, off, off},
-                    {off, on, on, on, on, on, on, off},
-                    {on, on, on, off, off, off, on, on},
-                    {on, on, on, off, off, off, on, on},
-                    {on, on, on, on, on, on, on, on},
-                    {on, on, on, on, on, on, on, on},
-                    {off, on, on, on, on, on, on, off},
-                    {off, off, off, off, off, off, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//happy
-void happy2() {
-  int eyes[8][8] = {
-                    {off, off, on, on, on, on, off, off},
-                    {off, on, on, on, on, on, on, off},
-                    {on, on, on, off, off, on, on, on},
-                    {on, on, on, off, off, on, on, on},
-                    {on, on, on, off, off, on, on, on},
-                    {on, on, on, on, on, on, on, on},
-                    {off, on, on, on, on, on, on, off},
-                    {off, off, off, off, off, off, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//happy v2 11/20/2016
-void happy3() {
-  int eyes[8][8] = {
-                   {off, off, off, on, on, off, off, off},
-                   {off, off, on, on, on, on, off, off},
-                   {off, on, on, on, on, on, on, off},
-                   {on, on, on, off, off, on, on, on},
-                   {on, on, off, off, off, off, on, on},
-                   {on, off, off, off, off, off, off, on},
-                   {off, off, off, off, off, off, off, off},
-                   {off, off, off, off, off, off, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//happy v2 11/20/2016
-void happy4() {
-    int eyes[8][8] = {
-                      {off, off, off, off, off, off, off, off},
-                      {off, off, off, on, on, off, off, off},
-                      {off, off, on, on, on, on, off, off},
-                      {off, on, on, on, on, on, on, off},
-                      {on, on, on, off, off, on, on, on},
-                      {on, on, off, off, off, off, on, on},
-                      {on, off, off, off, off, off, off, on},
-                      {off, off, off, off, off, off, off, off}
-    };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//sad
-void sad() {
-  int eyes[8][8] = {
-                     {off, off, off, off, off, off, off, off},
-                    {off, off, off, off, off, off, off, off},
-                    {on, off, off, off, off, off, off, on},
-                    {on, on, off, off, off, off, on, on},
-                    {on, on, on, off, off, on, on, on},
-                    {off, on, on, on, on, on, on, off},
-                    {off, off, on, on, on, on, off, off},
-                    {off, off, off, on, on, off, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//down
-void down() {
-  int eyes[8][8] = {
-                    {off, off, on, on, on, on, off, off},
-                    {off, on, on, on, on, on, on, off},
-                    {on, on, on, on, on, on, on, on},
-                    {on, on, on, on, on, on, on, on},
-                    {on, on, on, on, on, on, on, on},
-                    {on, on, on, off, off, on, on, on},
-                    {off, on, on, off, off, on, on, off},
-                    {off, off, on, on, on, on, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//blink
-void blink() {
-  int eyes[8][8] = {
-                    {off, off, off, off, off, off, off, off},
-                    {off, off, off, off, off, off, off, off},
-                    {off, on, on, on, on, on, on, off},
-                    {on, on, on, on, on, on, on, on},
-                    {off, on, on, on, on, on, on, off},
-                    {off, off, off, off, off, off, off, off},
-                    {off, off, off, off, off, off, off, off},
-                    {off, off, off, off, off, off, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//small eyes
-void small() {
-  int eyes[8][8] = {
-                    {off, off, off, off, off, off, off, off},
-                    {off, off, on, on, on, on, off, off},
-                    {off, on, on, on, on, on, on, off},
-                    {off, on, on, off, off, on, on, off},
-                    {off, on, on, off, off, on, on, off},
-                    {off, on, on, on, on, on, on, off},
-                    {off, off, on, on, on, on, off, off},
-                    {off, off, off, off, off, off, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
-//big eyes
-void neutral() {
-  int eyes[8][8] = {
-                    {off, off, on, on, on, on, off, off},
-                    {off, on, on, on, on, on, on, off},
-                    {on, on, on, on, on, on, on, on},
-                    {on, on, on, off, off, on, on, on},
-                    {on, on, on, off, off, on, on, on},
-                    {on, on, on, on, on, on, on, on},
-                    {off, on, on, on, on, on, on, off},
-                    {off, off, on, on, on, on, off, off}
-  };
-  memcpy(currentEyes, eyes, 8*8*sizeof(int));
-  setRow();
-  matrix.show();
-}
+
 void setRow() {
   for (int i = 0; i < 8; i++) {
     int val = currentEyes[0][i];
@@ -179,38 +36,6 @@ void setRow() {
   }
 }
 
-//eye combos
-void intro(){
-    neutral();
-    delay(500);
-    blink();
-    delay(200);
-    neutral();
-    delay(200);
-    blink();
-    delay(200);
-    neutral();
-}
-void happiness(){
-    happy4(); 
-    delay(200);
-    happy3();
-    delay(200);
-    happy4();
-    delay(200);
-    happy3();
-    delay(200);
-    happy4();
-    delay(200);
-    happy3();
-}
-void questioning() {
-    small();
-    delay(400);
-    neutral();
-    delay(400);
-    small();  
-}
 void blinking() {
    for (int i = 0; i < 8; i++) {
        matrix.drawLine(0, i, 7, i, off);
@@ -228,84 +53,20 @@ void blinking() {
       delay(25);
    }
 }
-// End Neopixel functions-------------------------
-void setup() {
-  // Neopixel setup
-  matrix.begin();
-  matrix.setBrightness(10);
 
-  // Slave setup
-  Serial.begin(9600);
+void receiveEvent() {
+  memcpy(currentEyes, eyesCalled, 8*8*sizeof(int));
+  setRow();
+  matrix.show();
 }
-void loop() {
-  currentMillis = millis();
-  
-  if (Serial.available()) {
-    receiveEvent(Serial.read() - '0');
-  }
-  delay(500);
-  
-  loopBlinking();
-}
-void receiveEvent(int x) {
 
-  // happy eye combo
-  if (x == 1) {
-    happiness();
+void resetStoredEyes() {
+   for (int row = 0; row < 8; row++) {
+    for (int col = 0; col < 8; col++) {
+      eyesCalled[row][col] = 0;
+    }
   }
-  // blink then question
-  else if (x == 2) {
-    blinking();
-    delay(800);
-    questioning();
-  }
-  // No
-  else if (x == 3) {
-    blinking();
-    delay(800);
-    sad();
-    delay(200);
-    small();
-    delay(3000);
-    neutral();
-  }
-  // Stress Question
-  else if (x == 4) {
-    neutral();
-  }
-  // Super Stressed
-  else if (x == 5) {
-    blinking();
-    delay(800);
-    sad();
-  }
-  // Pretty Stressed
-  else if (x == 6) {
-    blinking();
-    delay(200);
-    sad();
-  }
-  // Not too Stressed
-  else if (x == 7) {
-    blinking();
-    questioning();
-  }
-  // Almost no Stress
-  else if (x == 8) {
-    blinking();
-    happiness();
-  }
-  // Close 1
-  else if (x == 9) {
-    delay(2500);
-    
-    blinking();
-    happiness();
-  }
-  // default eyes
-  else if (x == 10) {
-    neutral();
-  }
+  receiveEvent();
 }
 
 void loopBlinking() {
@@ -313,4 +74,52 @@ void loopBlinking() {
       previousMillis = currentMillis;
       blinking();
   }
+}
+
+// End Neopixel functions-------------------------
+
+void setup() {
+  // Neopixel setup
+  matrix.begin();
+  matrix.setBrightness(10);
+
+  // Slave setup
+  Serial.begin(9600);
+  Serial.println("started sketch");
+}
+void loop() {
+  currentMillis = millis();
+
+  // reads in data from serial, expects format 101010101011, 1 being light on, 0 being light off
+  if (Serial.available()) {
+    delay(1); // allow input buffer to fill
+    resetStoredEyes();
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+        if (Serial.available()) {
+          int storedByte = (Serial.read() - '0'); // convert ascii to int
+          if (storedByte == 1) { // convert on to 255
+            storedByte = 255;
+          } else if (storedByte == 2) {
+            storedByte = -1;
+          } else if (storedByte == 3) {
+            storedByte = -255;
+          } else if (storedByte == 4) {
+            storedByte = -1020;
+          } else if (storedByte == 5) {
+            storedByte = -2040;
+          }
+          Serial.println(storedByte);
+          eyesCalled[row][col] = storedByte;
+        }
+      }
+    }
+    while (Serial.available()) { // clear serial buffer
+      Serial.read();
+    }
+    receiveEvent();
+  }
+  delay(500);
+  
+  loopBlinking();
 }
